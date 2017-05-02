70adb228c3f8895b399358698d1f5522d224bf9e
jassi <simran5590@gmail.com>
2017-05-01 20:06:54 -0700

    Fetched Data from live URL

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,7 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -32,11 +33,15 @@ import com.example.android.quakereport.Utils.QueryUtils;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity implements OnItemClickListener{
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
-    private ArrayList<EarthquakeInfo> earthquakes;
+
+    private List<EarthquakeInfo> earthquakes;
+    private final String USGS_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
+    private  EarthQuakeListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +49,13 @@ public class EarthquakeActivity extends AppCompatActivity implements OnItemClick
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-        earthquakes = QueryUtils.extractEarthquakes();
+        earthquakes = new ArrayList<>();
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a new {@link ArrayAdapter} of earthquakes
-        EarthQuakeListAdapter adapter = new EarthQuakeListAdapter(this, earthquakes);
+        adapter = new EarthQuakeListAdapter(this, earthquakes);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
@@ -58,6 +63,9 @@ public class EarthquakeActivity extends AppCompatActivity implements OnItemClick
 
         // Set the onclick event on the list item
         earthquakeListView.setOnItemClickListener(this);
+
+        new EarthQuakeInformationAsyncTask().execute(USGS_URL);
+
     }
 
     @Override
@@ -66,5 +74,31 @@ public class EarthquakeActivity extends AppCompatActivity implements OnItemClick
         EarthquakeInfo earthquakeInfo = earthquakes.get(position);
         Intent websiteIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(earthquakeInfo.getWebsite()));
         startActivity(websiteIntent);
+    }
+
+
+    private class EarthQuakeInformationAsyncTask extends AsyncTask<String, Void, List<EarthquakeInfo>>
+    {
+
+        @Override
+        protected List<EarthquakeInfo> doInBackground(String... params)
+        {
+            if(params.length != 1)
+            {
+                return null;
+            }
+
+            return QueryUtils.fetchEarthquakeData(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(List<EarthquakeInfo> earthquakeInfoList)
+        {
+            earthquakes = earthquakeInfoList;
+
+            adapter.clear();
+            adapter.addAll(earthquakes);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
